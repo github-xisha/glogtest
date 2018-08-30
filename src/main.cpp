a01c0618f1a1e1e0a64ea8d0efefa8952b3c6290
github-xisha <reader_xisha@163.com>
2018-08-30 21:04:51 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/********************************************************************
+  > File Name: main.cpp
+  > Author:xisha
+  > Mail: 1540348087@qq.com
+  > Created Time: 2018年08月30日 星期四 20时27分02秒
+********************************************************************/
+
+#include <glog/logging.h>
+#include <iostream>
+
+int main()
+{
+    LOG(INFO)<<"hello,world";
+}
